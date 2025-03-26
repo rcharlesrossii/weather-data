e998b72d3acbd2d94e7cd2b17bbf75df91faf84f
Charlie Ross (Richard Charles Ross II) <rcharlesrossii@gmail.com>
2025-03-26 13:50:16 -0500

    Update discord_notifier.py
    Edit so that my code is wrapped in a function, send_discord_update(), which will be utilized in main.py

--- discord_notifier.py
+++ discord_notifier.py
@@ -5,26 +5,41 @@ import os
 from weather_data import charlotte_weather, port_washington_weather
 
 
-# Use the MongoDB connection string
-DISCORD_WEBHOOK_URL = os.environ["discord_webhook_url"]
+def send_discord_update():
+    """
+    This function will utilize a Discord webhook to send a message to my
+    MongoDB Weather Database Discord server. Three different
+    conditions will trigger a message being sent:
+    1.) If the temperature is higher in Port Washington, WI than in
+    Charlotte, NC.
+    2.) If there is any recorded snowfall in Charlotte, NC.
+    3.) If there is more recorded snowfall in Charlotte, NC than in
+    Port Washington, WI.
 
-# Obtain the weather data for both Charlotte, NC & Port Washington, WI
-charlotte = charlotte_weather
-charlotte_temp = charlotte["temp"]
-charlotte_snow = charlotte["snow"]
+    Args:
+        None
 
-port_washington = port_washington_weather
-port_washington_temp = port_washington["temp"]
-port_washington_snow = port_washington["snow"]
-
-# If it is warmer in Port Washington, WI than in Charlotte, NC send a Discord message
-if charlotte_temp < port_washington_temp:
-    webhook = DiscordWebhook(url=DISCORD_WEBHOOK_URL, content=f"In Charlotte, NC it is currently {charlotte_temp}°F, and in Port Washington, WI it is currently {port_washington_temp}°F. Who would have thought it was warmer in Port Washington, WI than in Charlotte, NC today?")
-
-# If there is any amount of snow on the ground in Charlotte, NC send a Discord message
-if charlotte_snow != 0:
-    webhook = DiscordWebhook(url=DISCORD_WEBHOOK_URL, content=f"In Charlotte, NC there is currently {charlotte_snow} inches of snow on the ground. What a rare sight to see in Charlotte, NC!")
-
-# If there is more snow on the ground in Charlotte, NC than in Port Washington, WI send a Discord message
-if charlotte_snow > port_washington_snow:
-    webhook = DiscordWebhook(url=DISCORD_WEBHOOK_URL, content=f"In Charlotte, NC there is currently {charlotte_snow} inches of snow on the ground and in Port Washington, WI there is currently {port_washington_snow} inches of snow on the ground. How could there possibly more snow in Charlotte, NC than in Port Washignton, WI?")
+    Returns:
+        None
+    """
+    # Use the MongoDB connection string
+    DISCORD_WEBHOOK_URL = os.environ["discord_webhook_url"]
+    
+    # Obtain the weather data for both Charlotte, NC & Port Washington, WI
+    charlotte_temp = charlotte_weather["temp"]
+    charlotte_snow = charlotte_weather["snow"]
+    
+    port_washington_temp = port_washington_weather["temp"]
+    port_washington_snow = port_washington_weather["snow"]
+    
+    # If it is warmer in Port Washington, WI than in Charlotte, NC send a Discord message
+    if charlotte_temp < port_washington_temp:
+        webhook = DiscordWebhook(url=DISCORD_WEBHOOK_URL, content=f"In Charlotte, NC it is currently {charlotte_temp}°F, and in Port Washington, WI it is currently {port_washington_temp}°F. Who would have thought it was warmer in Port Washington, WI than in Charlotte, NC today?")
+    
+    # If there is any amount of snow on the ground in Charlotte, NC send a Discord message
+    if charlotte_snow != 0:
+        webhook = DiscordWebhook(url=DISCORD_WEBHOOK_URL, content=f"In Charlotte, NC there is currently {charlotte_snow} inches of snow on the ground. What a rare sight to see in Charlotte, NC!")
+    
+    # If there is more snow on the ground in Charlotte, NC than in Port Washington, WI send a Discord message
+    if charlotte_snow > port_washington_snow:
+        webhook = DiscordWebhook(url=DISCORD_WEBHOOK_URL, content=f"In Charlotte, NC there is currently {charlotte_snow} inches of snow on the ground and in Port Washington, WI there is currently {port_washington_snow} inches of snow on the ground. How could there possibly more snow in Charlotte, NC than in Port Washignton, WI?")
